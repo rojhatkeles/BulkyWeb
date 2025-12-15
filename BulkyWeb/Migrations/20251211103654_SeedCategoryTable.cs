5d9ee92353d1c15d2e775a11cbb434fc910676cd
UYSM-07 <UYSM-07@DESKTOP-2HS5TFB>
2025-12-15 11:33:15 +0300

    Tum hatalar giderildi ve proje yuklendi

---
+++ BulkyWeb/Migrations/20251211103654_SeedCategoryTable.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category21ID",
+                table: "Categories",
+                newName: "ID");
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "ID", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 2, "SciFi" },
+                    { 3, 3, "History" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Categories",
+                newName: "Category21ID");
+        }
+    }
+}
