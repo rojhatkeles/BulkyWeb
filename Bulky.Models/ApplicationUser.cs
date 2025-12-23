f86176a2f3add9e98ddf886b2f20d1e06942a54a
Rojhat Keleş <rojhatvedatkeles@gmail.com>
2025-12-23 12:32:11 +0300

    Initial commit

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -1,6 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
 namespace Bulky.Models;
 
-public class ApplicationUser
+public class ApplicationUser : IdentityUser
 {
+    [Required]
+    public int Number { get; set; }
     
+    public string? StreetAddress { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? PostalCode { get; set; }
+    public string Name { get; set; }
 }
--- Bulky.Utility/EmailSender.cs
+++ Bulky.Utility/EmailSender.cs
@@ -1,6 +1,15 @@
-namespace Bulky.Utility;
 
-public class EmailSender
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Bulky.Utility
 {
-    
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+        //logic to send email
+        return Task.CompletedTask;
+        }
+    }
 }
